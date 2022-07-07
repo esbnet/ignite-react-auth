c612b390b6712735ae50301ae589be9fced5a48d
Edmilson Soares <esbnet@gmail.com>
2022-07-07 17:25:46 -0300

    feat: validate permissions (server)

--- hooks/useCan.ts
+++ hooks/useCan.ts
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { validateUserPermissions } from "../utils/validateUserPermissions";
 
 type UserCanParams = {
 	permissions?: string[] | null | undefined;
@@ -13,25 +14,10 @@ export function useCan({ permissions, roles }: UserCanParams) {
 		return false;
 	}
 
-	//@ts-ignore
-	if (permissions?.length > 0) {
-		const hasAllPermissions = permissions?.every((permission) => {
-			return user?.permissions.includes(permission);
-		});
-		if (!hasAllPermissions) {
-			return false;
-		}
-	}
+	const userHasValidPermissions = validateUserPermissions({
+		//@ts-ignore
+		user, permissions, roles
+	});
 
-	//@ts-ignore
-	if (roles?.length > 0) {
-		const hasAllroles = roles?.some((role) => {
-			return user?.roles.includes(role);
-		});
-		if (!hasAllroles) {
-			return false;
-		}
-	}
-
-	return true;
+	return userHasValidPermissions;
 }
--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,3 +1,4 @@
+import decode from "jwt-decode";
 import {
 	GetServerSideProps,
 	GetServerSidePropsContext,
@@ -5,6 +6,7 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { validateUserPermissions } from "./validateUserPermissions";
 
 type WithSSRAuthOptions = {
 	permissions?: string[];
@@ -15,19 +17,40 @@ export function withSSRAuth<P>(
 	fn: GetServerSideProps<P>,
 	options?: WithSSRAuthOptions
 ) {
-    return async (
-        ctx: GetServerSidePropsContext
-        //@ts-ignore
+	return async (
+		ctx: GetServerSidePropsContext
+		//@ts-ignore
 	): Promise<GetServerSidePropsResult<P>> => {
 		const cookies = parseCookies(ctx);
+		const token = cookies["nextauth.token"];
+		const refreshToken = cookies["nextauth.refreshToken"];
 
-		if (!cookies["nextauth.token"]) {
+		if (!token) {
 			return {
 				redirect: {
 					destination: "/",
 					permanent: false,
 				},
 			};
+		}
+
+		if (options) {
+			const user = decode<{ permissions: string[]; roles: string[] }>(token);
+			const { permissions, roles } = options;
+			const userHasValidPermissions = validateUserPermissions({
+				user,
+				permissions,
+				roles,
+			});
+
+			if (!userHasValidPermissions) {
+				return {
+					redirect: {
+						destination: "/dashboard",
+						permanent: false,
+					},
+				};
+			}
 		}
 
 		try {
